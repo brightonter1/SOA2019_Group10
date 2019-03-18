0805b9fe0aefc9251801b0ccbb3a96c46e249d03
brightonter1 <59070035@it.kmitl.ac.th>
2019-03-18 08:28:18 +0700

    Add / Delete Category

--- src/main/java/com/demo/cosmetic/Category/CategoryController.java
+++ src/main/java/com/demo/cosmetic/Category/CategoryController.java
@@ -1,9 +1,9 @@
 package com.demo.cosmetic.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.PublicKey;
 import java.util.List;
 
 @RestController
@@ -14,4 +14,17 @@ public class CategoryController {
 
     @RequestMapping("/category")
     public List getAllCategory(){return categoryService.getAllCategories();}
+
+    // Add Category
+    @RequestMapping(value = "/category", method = RequestMethod.POST)
+    public String addCategories(@RequestBody String cate){
+        return categoryService.addCategories(new Category(cate));
+    }
+
+    // Remove Category
+    @RequestMapping(value = "/category", method = RequestMethod.DELETE)
+    public String removeCategories(@RequestBody String cate){
+        return categoryService.removeCategories(new Category(cate));
+    }
+
 }
