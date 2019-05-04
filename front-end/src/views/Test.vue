02361fd50de28d00430db018f844dc0917b0d385
Patcharaphorn Lervirul <kukkik.kpl@gmail.com>
2019-05-04 20:21:07 +0700

    Add axios and try getting data from API

---
+++ front-end/src/views/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="i in cosmetic" v-bind:key="i.id">
+        {{ i.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Test",
+  data() {
+    return {
+      cosmetic: []
+    };
+  },
+  mounted() {
+    this.fetchCosmetic();
+  },
+  methods: {
+    fetchCosmetic() {
+      axios.get("http://makeup-api.herokuapp.com/api/v1/products.json").then(
+        function(response) {
+          this.cosmetic = response.data;
+          console.log(response.data);
+        }.bind(this)
+      );
+    }
+  }
+};
+</script>
+
+<style scoped></style>
