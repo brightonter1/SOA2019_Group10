206a51c9a94352dfa20372b5bd441ed882dfac01
aiizxiifree <59070049@it.kmitl.ac.th>
2019-05-07 02:00:46 +0700

    add

--- front-end/src/functions/Wishlish.vue
+++ front-end/src/functions/Wishlish.vue
@@ -1,6 +1,18 @@
 <script>
+import Axios from 'axios';
 export default {
     methods:{
+        getUserWishlist(data){
+            Axios.post('path', data, {
+                headers:{
+                    'Authorization': localStorage.token
+                }
+            }).then(res=>{
+                return res.data
+            }).catch(e=>{
+                console.log(e)
+            })
+        }
     }
 }
 </script>
