476f713a42b265463ff522048bb5c59b8d2a01c8
aiizxiifree <59070017@it.kmitl.ac.th>
2019-05-07 02:26:40 +0700

    add f

--- front-end/src/functions/Wishlish.vue
+++ front-end/src/functions/Wishlish.vue
@@ -1,14 +1,39 @@
 <script>
-import Axios from 'axios';
+import Axios from 'axios'
+import variable from './Globalvar'
+const server = variable.data().server
 export default {
+    data(){
+        return{
+            listdata:[]
+        }
+    },
     methods:{
-        getUserWishlist(data){
-            Axios.post('path', data, {
+        getUserWishlish(){
+            Axios.get(server + 'wishlist/all').then(res=>{
+                this.listdata = res.data
+            }).catch(e=>{
+                console.log(e)
+            })
+        },
+        addWishlish(data){
+            Axios.post(server+ 'wishlist/add', data, {
                 headers:{
-                    'Authorization': localStorage.token
+                    'Authorization': 'Baerer ' + localStorage.token
                 }
             }).then(res=>{
-                return res.data
+                console.log(res)
+            }).catch(e=>{
+                console.log(e)
+            })
+        },
+         deleteWishlist(id){
+            Axios.post(server+ 'wishlist'+ id, {
+                headers:{
+                    'Authorization': 'Baerer ' + localStorage.token
+                }
+            }).then(res=>{
+                console.log(res)
             }).catch(e=>{
                 console.log(e)
             })
